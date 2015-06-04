9639da7e609cfd384ed9d8f39e15f3417ad25515
weam <weam_obaid_1@hotmail.com>
2015-06-04 10:03:19 -0700

    Changed the Java file

--- src/wordcount/WordCount.java
+++ src/wordcount/WordCount.java
@@ -3,7 +3,6 @@ package wordcount;
 public class WordCount {
 	
      public static void main(String[] args) {
-       System.out.println("Hello World");
- 
+       System.out.println("Hello World!");
     }
 }
